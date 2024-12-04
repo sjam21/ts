9d6227825a1df734f47bc02f5f95e5acdaa6fd97
sjam21 <srujanacharya69@gmail.com>
2024-12-04 12:04:21 +0530

    add styles and logs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+const list = document.getElementById("list");
+list.innerHTML = "<div>Hello</div>";
+
 async function getData(fetchURL: string) {
 		const response = await fetch(fetchURL);
 		const json = await response.json();
 		console.log(json);
+		setTimeout(() => console.log("Hello"), 1000)
+		list.innerHTML = "<div>Not Hello</div>";
 }
 
 getData("https://jsonplaceholder.typicode.com/posts");
-
-
